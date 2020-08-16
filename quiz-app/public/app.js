3bfe2f79d6812038939f54c105711f4ab36200e3
Arsalan Sheikh <41226869+ARSSHEIKH@users.noreply.github.com>
2020-08-16 12:55:33 +0500

    Add files via upload

--- quiz-app/public/app.js
+++ quiz-app/public/app.js
@@ -1,3 +1,12 @@
 function csharpQuiz(){
     window.location.href="csharp-quiz.html";
 }
+function jsQuiz(){
+    window.location.href="js-quiz.html";
+}
+function htmlQuiz(){
+    window.location.href="html-quiz.html";
+}
+function cssQuiz(){
+    window.location.href="css-quiz.html";
+}
